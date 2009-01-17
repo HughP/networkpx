602cf35baafd90e0ba912de544a1764ed58be96a
kennytm <kennytm@16a86fe7-a33b-0410-ab4e-5d08cd7d749e>
2009-01-17 19:20:11 +0000

    Fixed DEB generation to exclude .svn; Added file size to control files; Removed bogus ~/Library/iKeyEx/bin directory.

--- makedeb.sh
+++ makedeb.sh
@@ -1,28 +1,15 @@
 #!/bin/bash
 
-export COPYFILE_DISABLE=true
-export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
-
-# Ref: http://kb.adobe.com/selfservice/viewContent.do?externalId=tn_16831
-find . -name "*DS_Store" -depth -exec rm {} +
-
 for PackageName in `ls -d hk.*/`; do
 	if [[ -e "${PackageName%%/}/src/Makefile" ]]; then
-		cd "${PackageName%%/}/src/"
-		make
-		cd -
+		cd "${PackageName%%/}/src/";
+		make;
+		cd -;
 	fi
-	dpkg-deb -b ${PackageName%%/}/deb ../../${PackageName%%/}.deb;
+	./dpkg-deb-nodot ${PackageName%%/}/deb ../../${PackageName%%/}.deb;
 done
 
 dpkg-scanpackages -m ../../ /dev/null > ../../Packages;
 
+rm ../../Packages.bz2;
 bzip2 ../../Packages;
-
-# for PackageName in `ls -d hk.*/`; do
-#	if [[ -e "${PackageName%%/}/src/Makefile" ]]; then
-#		cd "${PackageName%%/}/src/"
-#		make clean
-#		cd -
-#	fi
-# done
