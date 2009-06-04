b83605f1d61f88f4a92d549b912b822ac3d26e3b
kennytm <kennytm@16a86fe7-a33b-0410-ab4e-5d08cd7d749e>
2009-06-04 15:33:34 +0000

    GriP Modal Table View: Hooked clickedMenuButton to allow dismiss by menu button, and restartDimTimer: to disable the lock screen auto going out.

--- hk.kennytm.grip/src/GPModalTableViewServer.m
+++ hk.kennytm.grip/src/GPModalTableViewServer.m
@@ -38,9 +38,64 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #import <AddressBook/AddressBook.h>
 #import <AddressBookUI/AddressBookUI.h>
 #import <GriP/GPModalTableViewController.h>
+#import <objc/runtime.h>
 
 static CFMutableDictionaryRef identifiedAlerts = NULL;
 static int currentAlertUID = 0;
+
+
+struct RCMBContext {
+	GPModalTableViewNavigationController* controller;
+	int currentAlertUID_copied;
+	unsigned currentDistance;
+};
+
+static void GPModalTableViewSearchForMostRecentIdentifiedAlert (int uid, GPModalTableViewNavigationController* controller, struct RCMBContext* context) {
+	unsigned distance = context->currentAlertUID_copied - uid;
+	if (distance < context->currentDistance) {
+		context->currentDistance = distance;
+		context->controller = controller;
+	}
+}
+
+static IMP original_clickedMenuButton = NULL, original_restartDimTimer = NULL;
+static void replaced_clickedMenuButton (id self, SEL _cmd) {
+	/*
+	 // doesn't work?
+	if (GPGetLocked())
+		original_clickedMenuButton(self, _cmd);
+	else
+	 */
+	{
+		struct RCMBContext context = {nil, currentAlertUID, UINT_MAX};
+		CFDictionaryApplyFunction(identifiedAlerts, (CFDictionaryApplierFunction)&GPModalTableViewSearchForMostRecentIdentifiedAlert, &context);
+		[context.controller sendDismissMessage];
+	}
+}
+static void replaced_restartDimTimer (id self, SEL _cmd, float duration) {
+	original_restartDimTimer(self, _cmd, 0x7f7fffff);	// == FLOAT_MAX
+}
+
+static void GPModalTableViewHookSpringBoard () {
+	// Hook some SpringBoard commands so that:
+	//  - Pressing the Home button will dismiss the active Modal Table View instead of closing the active app
+	//  - The lock screen will appear indefinitely until all Modal Table View are dismissed.
+	
+	if (CFDictionaryGetCount(identifiedAlerts) == 0) {
+		original_clickedMenuButton = method_setImplementation(class_getInstanceMethod(objc_getClass("SBUIController"), @selector(clickedMenuButton)), (IMP)&replaced_clickedMenuButton);
+		original_restartDimTimer = method_setImplementation(class_getInstanceMethod(objc_getClass("SBAwayController"), @selector(restartDimTimer:)), (IMP)&replaced_restartDimTimer);
+	}
+}
+
+static void GPModalTableViewUnhookSpringBoard() {
+	if (CFDictionaryGetCount(identifiedAlerts) == 0) {
+		method_setImplementation(class_getInstanceMethod(objc_getClass("SBUIController"), @selector(clickedMenuButton)), original_clickedMenuButton);
+		Class _SBAwayController = objc_getClass("SBAwayController");
+		method_setImplementation(class_getInstanceMethod(_SBAwayController, @selector(restartDimTimer:)), original_restartDimTimer);
+		original_restartDimTimer(objc_msgSend(_SBAwayController, @selector(sharedAwayController)), @selector(restartDimTimer:), 0x41000000);
+	}
+}
+
 
 struct GPTVAActuallyShowAlert_Context {
 	NSArray* array;
@@ -48,6 +103,8 @@ struct GPTVAActuallyShowAlert_Context {
 };
 
 static void GPTVAActuallyShowAlert(CFRunLoopTimerRef timer, struct GPTVAActuallyShowAlert_Context* context) {
+	GPModalTableViewHookSpringBoard();
+	
 	NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
 	GPModalTableViewNavigationController* navCtrler = [[GPModalTableViewNavigationController alloc] initWithDictionary:[context->array objectAtIndex:1]];
 	navCtrler.uid = [NSNumber numberWithInt:context->alertUID];
@@ -98,8 +155,6 @@ CFDataRef GPModalTableViewServerCallback (CFMessagePortRef serverPort, SInt32 ty
 																	  &timerContext);
 			CFRunLoopAddTimer(CFRunLoopGetCurrent(), constructorTimer, kCFRunLoopCommonModes);
 			CFRelease(constructorTimer);
-						
-			//[GPTVAActuallyShowAlert performSelectorInBackground:@selector(show:) withObject:[NSArray arrayWithObjects:array, [NSNumber numberWithInt:alertUID], nil]];// waitUntilDone:NO];
 			
 			retData = CFDataCreate(NULL, (const UInt8*)&alertUID, sizeof(int));
 			break;
@@ -154,6 +209,7 @@ CFDataRef GPModalTableViewServerCallback (CFMessagePortRef serverPort, SInt32 ty
 			GPServerForwardMessage(string, type, data, NULL);
 			CFRelease(data);
 			CFRelease(string);
+			GPModalTableViewUnhookSpringBoard();
 			break;
 		}
 			
